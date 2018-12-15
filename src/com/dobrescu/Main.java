6cb4dbd08fe300577dd422a8c1ce179133528b02
Alex Dobrescu <alexdobrescu@icloud.com>
2018-12-15 19:51:32 +0200

    Am comis-o

---
+++ src/com/dobrescu/Main.java
@@ -0,0 +1,18 @@
+package com.dobrescu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver= true;
+        int score= 5000;
+        int levelCompleted=5;
+        int bonus= 100;
+
+        if(score<=5000){
+            System.out.println("your score was less than 5000");
+        }else{
+            System.out.println("got here");
+        }
+    }
+}
